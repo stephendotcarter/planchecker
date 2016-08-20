502b065a4dfa5587cb61b8e66e00ba645a625e55
Stephen Carter <stephencarter@gmail.com>
2016-08-20 11:17:12 +0100

    Make indentation checks less stringent

--- plan/plan.go
+++ plan/plan.go
@@ -909,8 +909,12 @@ func (e *Explain) parseline(line string) error {
 		// Parse a new node
 		newNode := e.createNode(line)
 
-		if len(e.Nodes) == 0 && newNode.Indent != 1 {
-			return errors.New(fmt.Sprintf("Wrong indentation on first plan node... expected 1, found %d:\n%s\nRecommend running EXPLAIN again and resubmitting the plan.\nDo not manually adjust the indentation as this will lead to incorrect parsing!\n", newNode.Indent, strings.TrimRight(line, " ")))
+		if len(e.Nodes) == 0 && newNode.Indent > 1 {
+			return errors.New(fmt.Sprintf("Detected wrong indentation on first plan node:\n%s\n\nRecommend running EXPLAIN again and resubmitting the plan.\nDo not manually adjust the indentation as this will lead to incorrect parsing!\n", strings.TrimRight(line, " ")))
+		}
+
+		if len(e.Nodes) > 0 && newNode.Indent < 2 {
+			return errors.New(fmt.Sprintf("Detected wrong indentation on line:\n%s\n\nRecommend running EXPLAIN again and resubmitting the plan.\nDo not manually adjust the indentation as this will lead to incorrect parsing!\n", strings.TrimRight(line, " ")))
 		}
 
 		// If this is the first node then insert the TopPlan also
